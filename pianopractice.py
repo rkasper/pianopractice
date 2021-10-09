4c6855b2b38a7c451884f2af438106b504185a2e
Richard Kasperowski <r@kasperowski.com>
2021-10-09 17:37:44 -0400

    Rename that new data object

--- pianopractice.py
+++ pianopractice.py
@@ -2,7 +2,7 @@
 import random
 
 
-class PianoPractice:
+class Activity:
     group : str
     exercise : str
 
@@ -13,29 +13,30 @@ class PianoPractice:
     def __repr__(self):
         return self.group + ': ' + self.exercise
 
+class PianoPractice:
     @staticmethod
     def exercises_to_practice() -> [str]:
-        scales: [str] = [PianoPractice("Scale", "Major"), PianoPractice("Scale", "Blues (minor)"),
-                         PianoPractice("Scale", "Blues (major)"), PianoPractice("Scale", "Mixolydian (dom7)")]
-        hanon: [str] = [PianoPractice("Hanon", "1"), PianoPractice("Hanon", "2"), PianoPractice("Hanon", "3"),
-                        PianoPractice("Hanon", "4"), PianoPractice("Hanon", "5"), PianoPractice("Hanon", "6"),
-                        PianoPractice("Hanon", "7"), PianoPractice("Hanon", "8"), PianoPractice("Hanon", "9"),
-                        PianoPractice("Hanon", "10"), PianoPractice("Hanon", "11"), PianoPractice("Hanon", "12"),
-                        PianoPractice("Hanon", "13"), PianoPractice("Hanon", "14"), PianoPractice("Hanon", "15"),
-                        PianoPractice("Hanon", "16"), PianoPractice("Hanon", "17"), PianoPractice("Hanon", "18"),
-                        PianoPractice("Hanon", "19"), PianoPractice("Hanon", "20"), PianoPractice("Hanon", "21"),
-                        PianoPractice("Hanon", "22"), PianoPractice("Hanon", "23"), PianoPractice("Hanon", "24"),
-                        PianoPractice("Hanon", "25"), PianoPractice("Hanon", "26"), PianoPractice("Hanon", "27"),
-                        PianoPractice("Hanon", "28"), PianoPractice("Hanon", "29"), PianoPractice("Hanon", "30")]
-        blues_school: [str] = [PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, The First Lesson"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #1"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #2"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #3"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #4"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #5"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #6"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #7"),
-                               PianoPractice("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #8")]
+        scales: [str] = [Activity("Scale", "Major"), Activity("Scale", "Blues (minor)"),
+                         Activity("Scale", "Blues (major)"), Activity("Scale", "Mixolydian (dom7)")]
+        hanon: [str] = [Activity("Hanon", "1"), Activity("Hanon", "2"), Activity("Hanon", "3"),
+                        Activity("Hanon", "4"), Activity("Hanon", "5"), Activity("Hanon", "6"),
+                        Activity("Hanon", "7"), Activity("Hanon", "8"), Activity("Hanon", "9"),
+                        Activity("Hanon", "10"), Activity("Hanon", "11"), Activity("Hanon", "12"),
+                        Activity("Hanon", "13"), Activity("Hanon", "14"), Activity("Hanon", "15"),
+                        Activity("Hanon", "16"), Activity("Hanon", "17"), Activity("Hanon", "18"),
+                        Activity("Hanon", "19"), Activity("Hanon", "20"), Activity("Hanon", "21"),
+                        Activity("Hanon", "22"), Activity("Hanon", "23"), Activity("Hanon", "24"),
+                        Activity("Hanon", "25"), Activity("Hanon", "26"), Activity("Hanon", "27"),
+                        Activity("Hanon", "28"), Activity("Hanon", "29"), Activity("Hanon", "30")]
+        blues_school: [str] = [Activity("Blues School", "Major Blues 12-Bar Form & Harmony, The First Lesson"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #1"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #2"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #3"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #4"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #5"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #6"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #7"),
+                               Activity("Blues School", "Major Blues 12-Bar Form & Harmony, Variation #8")]
         return [random.choice(scales), random.choice(hanon), random.choice(blues_school)]
 
     @staticmethod
