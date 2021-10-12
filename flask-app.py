9849f8592d0acba4cb3708f5288d22b8f67a9f0f
Richard Kasperowski <r@kasperowski.com>
2021-10-12 18:36:48 -0400

    em dash — yay!

--- flask-app.py
+++ flask-app.py
@@ -10,7 +10,7 @@ def home_page():  # put application's code here
     page = '<!DOCTYPE html><html><head><title>PianoPlay.app</title></head>'
     page += '<body><h1>PianoPlay</h1>'
     page += '<p>You can become the pianist you''ve always wanted to be!</p>'
-    page += '<p>Practice your piano exercises just right: the right exercises at the right times; just the right amount of exercise to keep it fun, keep you advancing, and leave you time for the fun stuff on piano&em;playing songs and making music!</p>'
+    page += '<p>Practice your piano exercises just right: the right exercises at the right times; just the right amount of exercise to keep it fun, keep you advancing, and leave you time for the fun stuff on piano&mdash;playing songs and making music!</p>'
     page += '<h2>Practice these exercises:</h2>'
     page += '<ul>'
     for exercise in PianoPractice.exercises_to_practice():
