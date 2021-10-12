bf98c4c6bb3bd32c9bafac45a1c9fdeda6380d53
Richard Kasperowski <r@kasperowski.com>
2021-10-12 18:18:47 -0400

    New app name and product vision

--- flask-app.py
+++ flask-app.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():  # put application's code here
-    page = '<!DOCTYPE html><html><head><title>Piano Practice</title></head><body><h1>Yay Piano Exercises!</h1>'
+    page = '<!DOCTYPE html><html><head><title>PianoPlay.app</title></head>'
+    page += '<body><h1>PianoPlay</h1>'
+    page += '<p>You can become the pianist you''ve always wanted to be!</p>'
+    page += '<p>Practice your piano exercises just right: the right exercises at the right times; just the right amount of exercise to keep it fun, keep you advancing, and leave you time for the fun stuff on piano&em;playing songs and making music!</p>'
     page += '<h2>Practice these exercises:</h2>'
     page += '<ul>'
     for exercise in PianoPractice.exercises_to_practice():
         page += '<li><a href="' + exercise.url + '">' + exercise.group + ': ' + exercise.name + '</a></li>'
     page += '<li>... and a new exercise</li>'
     page += '</ul>'
-    page += '<h2>Practice the 12 keys in this sequence:</h2>'
+    page += '<h2>In this sequence of keys:</h2>'
     page += '<ul><li>'
     page += '<li>'.join(map(str, PianoPractice.keys_to_practice()))
     page += '</ul></body></html>'
