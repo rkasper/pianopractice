2eaa0b96181d57987b7039511fad56cc149b5972
Richard Kasperowski <r@kasperowski.com>
2021-10-14 17:34:12 -0400

    Add chromatic scale

--- pianopractice.py
+++ pianopractice.py
@@ -10,7 +10,8 @@ class PianoPractice:
         scales: [str] = [Activity("Scale", "Major", "https://pianoscales.org/major.html"),
                          Activity("Scale", "Blues (minor)", "https://pianoscales.org/blues.html"),
                          Activity("Scale", "Blues (major)", "https://pianoscales.org/blues.html"),
-                         Activity("Scale", "Mixolydian (dom7)", "https://pianoscales.org/mixolydian.html")]
+                         Activity("Scale", "Mixolydian (dom7)", "https://pianoscales.org/mixolydian.html"),
+                         Activity("Scale", "Chromatic", "https://www.pianoscales.org/chromatic.html")]
         hanon: [str] = [Activity("Hanon", "1", "https://www.hanon-online.com/the-virtuoso-pianist-part-i/hanon-exercise-n-1/"),
                         Activity("Hanon", "2", "https://www.hanon-online.com/the-virtuoso-pianist-part-i/hanon-exercise-n-2/"),
                         Activity("Hanon", "3", "https://www.hanon-online.com/the-virtuoso-pianist-part-i/hanon-exercise-n-3/"),
