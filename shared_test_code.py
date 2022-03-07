c0fce357647b8f0d365e07dc95b4ad239396ec52
Richard Kasperowski <r@kasperowski.com>
2022-03-07 10:24:51 -0500

    Explanation and credit

--- shared_test_code.py
+++ shared_test_code.py
@@ -1,6 +1,12 @@
 import warnings
 
 
+# The boto S3 storage API doesn't close socket connections. This is intentional, and it's OK. But the unittest runner
+# emits warning messages about this, which makes it harder to read test results. Wrap tests in @ignore_warnings to
+# silence the warnings and make test results easier to read. Be careful with this. ;-)
+# I borrowed this code from
+# https://www.alanwsmith.com/posts/20eops3yw16k--suppress-python-mysql-connector-deprecationwarning-and-resourcewarnings-in-unittests
+# Thanks, friend!
 def ignore_warnings(test_method):
     def test_run(self, *args, **kwargs):
         with warnings.catch_warnings():
