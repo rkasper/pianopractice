332f72b9122096915fdd8518e0243eef73f334d0
Richard Kasperowski <r@kasperowski.com>
2022-03-06 21:10:56 -0500

    Try to ignore boto warnings

--- test_storage.py
+++ test_storage.py
@@ -1,11 +1,21 @@
 import json
 import unittest
+import warnings
 from os import environ
 
 from storage import Storage
 
 
 class StorageTests(unittest.TestCase):
+    def ignore_warnings(test_method):
+        def test_run(self, *args, **kwargs):
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore')
+                test_method(self, *args, **kwargs)
+
+        return test_run
+
+    @ignore_warnings
     def test_canGetScalesFromStorage(self):
         content_as_string = Storage.get_scales_as_string()
         self.assertTrue(len(content_as_string) > 0)
@@ -15,6 +25,7 @@ class StorageTests(unittest.TestCase):
 
         self.assertEqual(json.loads(content_as_string), content_as_json)
 
+    @ignore_warnings
     def test_canGetHanonFromStorage(self):
         content_as_string = Storage.get_hanon_as_string()
         self.assertTrue(len(content_as_string) > 0)
@@ -24,6 +35,7 @@ class StorageTests(unittest.TestCase):
 
         self.assertEqual(json.loads(content_as_string), content_as_json)
 
+    @ignore_warnings
     def test_canGetBluesFromStorage(self):
         content_as_string = Storage.get_blues_as_string()
         self.assertTrue(len(content_as_string) > 0)
@@ -33,6 +45,7 @@ class StorageTests(unittest.TestCase):
 
         self.assertEqual(json.loads(content_as_string), content_as_json)
 
+    @ignore_warnings
     @unittest.skipUnless(environ.get('MOCK_DB'), "Only test with mock DB - don't break the production DB.")
     def test_canSetScalesToStorage(self):
         original_content = Storage.get_scales_as_string()
@@ -41,6 +54,7 @@ class StorageTests(unittest.TestCase):
         self.assertEqual(new_content, Storage.get_scales_as_string())
         Storage.set_scales_from_string(original_content)
 
+    @ignore_warnings
     @unittest.skipUnless(environ.get('MOCK_DB'), "Only test with mock DB - don't break the production DB.")
     def test_canSetHanonToStorage(self):
         original_content = Storage.get_hanon_as_string()
@@ -49,6 +63,7 @@ class StorageTests(unittest.TestCase):
         self.assertEqual(new_content, Storage.get_hanon_as_string())
         Storage.set_hanon_from_string(original_content)
 
+    @ignore_warnings
     @unittest.skipUnless(environ.get('MOCK_DB'), "Only test with mock DB - don't break the production DB.")
     def test_canSetBluesToStorage(self):
         original_content = Storage.get_blues_as_string()
