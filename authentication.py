81b17dc980ec09c0a47bbd0099530be45d774dce
Richard Kasperowski <r@kasperowski.com>
2022-04-25 12:47:59 -0400

    A backup

--- authentication.py
+++ authentication.py
@@ -32,9 +32,6 @@ def did_token_required(func):
                     did_token = request.args.get('didt')
                 else:
                     did_token = request.form.get('didt')
-                print('inject_did_token: did_token: ' + str(did_token))
-                auth_header = request.headers.get('Authorization')
-                print('inject_did_token: auth_header: ' + str(auth_header))
 
                 magic = Magic(api_secret_key=__get_magic_secret_key())
 
