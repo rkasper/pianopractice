e35dd365dfed2158942e9e697f5e2057e499341d
Richard Kasperowski <r@kasperowski.com>
2022-08-06 21:37:53 -0400

    Refactor authentication back to authentication.py

--- app/authentication.py
+++ app/authentication.py
@@ -24,19 +24,31 @@ def did_token_required(func):
         test_mode = os.getenv('TEST_MODE')
         did_token = ''  # Because even if we're in test-mode, we pass did_token to the render template.
         if test_mode is None or test_mode == 'FALSE':
+            # TODO Rename didt to be magic_credential in the .html files.
             try:
-                # did_token is the login authorization token from Magic.
+                # The login authorization token from Magic is either did_token or magic_credential. magic_credential
+                # seems to be the parameter to look for today; did_token worked in the past.
                 # We're passing the auth token to the form, and back from the form to this route. There's probably a
                 # better way to stay logged in, but this works well enough for now.
                 if request.method == 'GET':
+                    print('GET')
                     did_token = request.args.get('didt')
+                    print('from didt: did_token is ' + str(did_token))
+                    if did_token is None:
+                        did_token = request.args.get('magic_credential')
+                    print('from magic_credential: did_token is ' + str(did_token))
                 else:
+                    print('POST')
                     did_token = request.form.get('didt')
-
-                magic = Magic(api_secret_key=__get_magic_secret_key())
+                    print('from didt: did_token is ' + str(did_token))
+                    if did_token is None:
+                        did_token = request.args.get('magic_credential')
+                    print('from magic_credential: did_token is ' + str(did_token))
 
                 # Validate the did_token
+                magic = Magic(api_secret_key=__get_magic_secret_key())
                 magic.Token.validate(did_token)
+                print('--- did_token is valid ---')
 
                 # Sample code: The Magic docs suggest using issuer or public_address as the key for storing and
                 # retrieving user data in my app. In this app, we might store/retrieve a user-specific list of piano
@@ -59,6 +71,7 @@ def did_token_required(func):
                 # TODO Consider changing this return statement - it depends on Flask running, which gives us a
                 # dependency we don't necessarily want. It's not so bad, because we only invoke authentication within
                 # Flask, but it makes it harder to test, if nothing else.
+                print(e)
                 return redirect(url_for("login"))
         else:
             return func(did_token)
--- flask-app.py
+++ flask-app.py
@@ -1,9 +1,6 @@
-import os
-
 from flask import Flask, render_template, request
-from magic_admin import Magic
 
-from app.authentication import did_token_required, with_magic_publishable_api_key, __get_magic_secret_key
+from app.authentication import did_token_required, with_magic_publishable_api_key
 from app.pianopractice import PianoPractice
 from app.storage import Storage
 
@@ -32,70 +29,27 @@ def login(magic_publishable_api_key):
 
 
 @app.route('/admin', methods=['GET', 'POST'])
-#@did_token_required
-#def admin(did_token):
-@with_magic_publishable_api_key
-def admin(magic_publishable_api_key):
+@did_token_required
+def admin(did_token):
     print('admin')
+    if request.method == 'POST':  # The web form supplied the data. Store the new data.
+        scales = str(request.form.get(Storage.STORAGE_KEY_SCALES))
+        hanon = str(request.form.get(Storage.STORAGE_KEY_HANON))
+        blues = str(request.form.get(Storage.STORAGE_KEY_BLUES))
 
-    # TODO Simplify this:
-    # - Get the did_token, create the Magic instance, validate the did_token.
-    # - If the did_token is valid, return render_template("admin.html")
-    # - Else return render_template("login.html")
-    try:
-        # did_token is the login authorization token from Magic.
-        # We're passing the auth token to the form, and back from the form to this route. There's probably a
-        # better way to stay logged in, but this works well enough for now.
-        if request.method == 'GET':
-            print('GET')
-            did_token = request.args.get('didt')
-            print('from didt: did_token is ' + str(did_token))
-            if (did_token == None):
-                did_token = request.args.get('magic_credential')
-            print('from magic_credential: did_token is ' + str(did_token))
-        else:
-            print('POST')
-            did_token = request.form.get('didt')
-            print('from didt: did_token is ' + str(did_token))
-            if (did_token == None):
-                did_token = request.args.get('magic_credential')
-            print('from magic_credential: did_token is ' + str(did_token))
+        Storage.set_scales_from_string(scales)
+        Storage.set_hanon_from_string(hanon)
+        Storage.set_blues_from_string(blues)
+    else:  # Get the data from storage.
+        scales = Storage.get_scales_as_string()
+        hanon = Storage.get_hanon_as_string()
+        blues = Storage.get_blues_as_string()
 
-        # Validate the did_token
-        magic = Magic(api_secret_key=__get_magic_secret_key())
-        magic.Token.validate(did_token)
-        print('--- did_token is valid ---')
-
-        # DID token is valid - go ahead and render the admin page
-        if request.method == 'POST':  # The web form supplied the data. Store the new data.
-            scales = str(request.form.get(Storage.STORAGE_KEY_SCALES))
-            hanon = str(request.form.get(Storage.STORAGE_KEY_HANON))
-            blues = str(request.form.get(Storage.STORAGE_KEY_BLUES))
-
-            Storage.set_scales_from_string(scales)
-            Storage.set_hanon_from_string(hanon)
-            Storage.set_blues_from_string(blues)
-        else:  # Get the data from storage.
-            scales = Storage.get_scales_as_string()
-            hanon = Storage.get_hanon_as_string()
-            blues = Storage.get_blues_as_string()
-
-        return render_template("admin.html",
-                               # didt=did_token,
-                               magic_publishable_api_key=magic_publishable_api_key,
-                               scales=scales,
-                               hanon=hanon,
-                               blues=blues)
-
-    except Exception as e:
-        # DID token is invalid. Login.
-        #return redirect(url_for("login"))
-        print(e)
-        print('--- did_token is invalid ---')
-        return render_template("login.html",
-                               magic_publishable_api_key=magic_publishable_api_key)
-
-
+    return render_template("admin.html",
+                           didt=did_token,
+                           scales=scales,
+                           hanon=hanon,
+                           blues=blues)
 
 
 if __name__ == '__main__':
